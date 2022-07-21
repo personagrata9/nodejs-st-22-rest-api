159a1366cf9677501a616f36d10b929bc0a6ebf1
Iryna Bakhur <ira.bakhur@icloud.com>
2022-07-21 12:55:38 +0300

    feat: implement user password and user age validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,23 @@
-import { IsString, IsInt, IsBoolean } from 'class-validator';
+import { IsString, IsInt, IsBoolean, Min, Max } from 'class-validator';
+import { HasLetter } from '../validation-decorators/has-letter.decorator';
+import { HasNumber } from '../validation-decorators/has-number.decorator';
 
 export class CreateUserDto {
   @IsString()
   login: string;
 
   @IsString()
+  @HasLetter('password', {
+    message: 'password must contain at least one letter',
+  })
+  @HasNumber('password', {
+    message: 'password must contain at least one number',
+  })
   password: string;
 
   @IsInt()
+  @Min(4)
+  @Max(130)
   age: number;
 
   @IsBoolean()
---
+++ src/users/validation-decorators/has-number.decorator.ts
@@ -0,0 +1,31 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator';
+
+export function HasNumber(
+  property: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'HasNumber',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+          return (
+            typeof value === 'string' &&
+            typeof relatedValue === 'string' &&
+            /\d/.test(value)
+          );
+        },
+      },
+    });
+  };
+}
