f60e4189077b34b196a071cb79fb925798027dd9
Iryna Bakhur <ira.bakhur@icloud.com>
2022-08-16 00:41:03 +0300

    refactor: update error filter

--- src/common/filters/error.filter.ts
+++ src/common/filters/error.filter.ts
@@ -25,7 +25,7 @@ export class ErrorFilter implements ExceptionFilter {
     this.logger = WinstonLogger.getInstance();
   }
 
-  catch(exception: HttpException | Error, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
@@ -35,20 +35,27 @@ export class ErrorFilter implements ExceptionFilter {
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const message: string | string[] =
-      exception instanceof HttpException
-        ? (exception.getResponse() as ErrorResponse).message
-        : exception.message;
+    const message: string | string[] = this.getMessage(exception);
 
     const error: string =
       exception instanceof HttpException
-        ? (exception.getResponse() as ErrorResponse).error
+        ? exception.name
         : 'Internal Server Error';
 
     res.status(statusCode).json({ statusCode, message, error });
 
     this.logError(statusCode, error, message, req, res);
   }
+
+  private getMessage = (exception: unknown): string | string[] => {
+    if (exception instanceof HttpException) {
+      return (exception.getResponse() as ErrorResponse).message;
+    } else if (exception instanceof Error) {
+      return exception.message;
+    } else {
+      return 'Something went wrong!';
+    }
+  };
 
   private logError = (
     statusCode: number,
